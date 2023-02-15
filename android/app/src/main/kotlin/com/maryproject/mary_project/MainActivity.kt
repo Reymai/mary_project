d7f33facf65cfc9c20d96112e964b5491ff6c1ec
Reymai <ricosigla@gmail.com>
2023-02-15 21:46:49 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/maryproject/mary_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.maryproject.mary_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:mary_project/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
